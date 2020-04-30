914d3d2bbd57e86b5f38d78f6607e1c1fe5ac03f
Ruhong <seahrh@gmail.com>
2020-04-30 15:14:33 +0800

    add setup.py, src layout

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0"
+setup(
+    name="acme.fraudcop.serving",
+    version=__version__,
+    python_requires="==3.7.*",
+    install_requires=[],
+    extras_require={"tests": ["black~=19.10b0", "mypy>=0.770", "pytest>=5.4.1"]},
+    packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    description="acme fraudcop serving, pre and post-processing",
+    license="All rights reserved",
+    author="seahrh",
+    author_email="seahrh@gmail.com",
+    url="https://github.com/seahrh/acme.fraudcop.serving",
+)
